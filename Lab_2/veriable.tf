7b71d81d8343c476294b97ae5da657ce17b9cdaf
Doublex Nwankou <moudoblex@yahoo.com>
2025-07-24 20:13:55 +0100

    added vnet and subnet files

--- Lab_2/veriable.tf
+++ Lab_2/veriable.tf
@@ -1,15 +1,19 @@
+# current subscription
 variable "subscription_id" {
   type = string
 }
 
+# Business unit or application
 variable "application_name" {
   type = string
 }
 
+# Environment
 variable "environment_name" {
   type = string
 }
 
+# location for deployment
 variable "primary_location" {
   type = string
 }
